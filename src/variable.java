8cbb38a97383484c7b2d8d35e2a01e5d3e0e9063
Trung <trung@example.com>
2025-12-03 20:36:46 +0700

    variable.java

---
+++ src/variable.java
@@ -0,0 +1,17 @@
+public class variable {
+     public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double  d = 20.5;
+        boolean bo = true;
+        char c = 't';
+        String str = "Thanh Thùy";
+         System.out.println(str);
+         System.out.println(i);
+         System.out.println(f);
+         System.out.println(d);
+         System.out.println(bo);
+         System.out.println(c);
+    }
+};
+
